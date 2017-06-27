f88522d7da939f30c83d9ff42c0eeef314685c27
NadimRai <rai_nadim@yahoo.com>
2017-06-26 18:18:12 -0700

    generated migration to combine items in cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626071959) do
+ActiveRecord::Schema.define(version: 20170627002402) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
