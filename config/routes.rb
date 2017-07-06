7830fbf6caa685173f4b82a2c5e95c004106452a
NadimRai <rai_nadim@yahoo.com>
2017-07-05 20:30:26 -0700

    limiting access login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  get 'admin' => 'admin#index'
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
   resources :users
   resources :orders
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   resources :products do
     get :who_bought, on: :member
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
 end
